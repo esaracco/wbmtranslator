9c02cc7b098278650f1123dda225dcb9705eed92
Emmanuel Saracco <emmanuel@esaracco.fr>
2019-06-05 16:17:26 +0200

    Add files via upload

---
+++ data/reverso_languages.pm
@@ -0,0 +1,9 @@
+# reverso.net languages
+our %reverso_langs = (
+     'Allemand' => 'de',
+     'Anglais' => 'en',
+     'Espagnol' => 'es',
+     'Français' => 'fr',
+     'Italien' => 'it',
+     'Portugais' => 'pt'
+);
